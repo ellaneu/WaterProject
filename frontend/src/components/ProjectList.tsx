b94f26db4a16af3beee05214511d58873d2ebc98
Ella Neumarker <eneumarker@gmail.com>
2025-03-23 22:04:49 -0600

    Added Cart Functionality

--- frontend/src/components/ProjectList.tsx
+++ frontend/src/components/ProjectList.tsx
@@ -1,22 +1,25 @@
 import { useEffect, useState } from 'react';
-import { Project } from './types/Project';
+import { Project } from '../types/Project';
+import { useNavigate } from 'react-router-dom';
 
-function ProjectList({selectedCategories}: {selectedCategories: string[]}) {
+function ProjectList({ selectedCategories }: { selectedCategories: string[] }) {
   const [projects, setProjects] = useState<Project[]>([]);
   const [pageSize, setPageSize] = useState<number>(10);
   const [pageNum, setPageNum] = useState<number>(1);
   const [totalItems, setTotalItems] = useState<number>(0);
   const [totalPages, setTotalPages] = useState<number>(0);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchProjects = async () => {
-
-      const categoryParams = selectedCategories.map((cat) => `projectTypes=${encodeURIComponent(cat)}`).join('&')
+      const categoryParams = selectedCategories
+        .map((cat) => `projectTypes=${encodeURIComponent(cat)}`)
+        .join('&');
 
       const response = await fetch(
         `https://localhost:5000/Water/AllProjects?pageHowMany=${pageSize}&pageNum=${pageNum}${selectedCategories.length ? `&${categoryParams}` : ''}`,
         {
-          credentials: 'include'
+          credentials: 'include',
         }
       );
       const data = await response.json();
@@ -50,31 +53,42 @@ function ProjectList({selectedCategories}: {selectedCategories: string[]}) {
                 <strong>Project Status:</strong> {p.projectFunctionalityStatus}
               </li>
             </ul>
+
+            <button className='btn btn-success' onClick={() => navigate(`/donate/${p.projectName}/${p.projectId}`)}>Donate</button>
           </div>
         </div>
       ))}
 
-      <button disabled={pageNum === 1} onClick={() => setPageNum(pageNum - 1)}>Previous</button>
+      <button disabled={pageNum === 1} onClick={() => setPageNum(pageNum - 1)}>
+        Previous
+      </button>
 
       {[...Array(totalPages)].map((_, index) => (
-        <button key={index + 1} onClick={() => setPageNum(index + 1)} disabled={pageNum === index + 1}>
+        <button
+          key={index + 1}
+          onClick={() => setPageNum(index + 1)}
+          disabled={pageNum === index + 1}
+        >
           {index + 1}
         </button>
       ))}
 
-      <button disabled={pageNum === totalPages} onClick={() => setPageNum(pageNum + 1)}>Next</button>
+      <button
+        disabled={pageNum === totalPages}
+        onClick={() => setPageNum(pageNum + 1)}
+      >
+        Next
+      </button>
 
       <br />
       <label>
         Results per page:
         <select
           value={pageSize}
-          onChange=
-            {(p) => {
-              setPageSize(Number(p.target.value));
-              setPageNum(1);
-            }
-          }
+          onChange={(p) => {
+            setPageSize(Number(p.target.value));
+            setPageNum(1);
+          }}
         >
           <option value="5">5</option>
           <option value="10">10</option>
