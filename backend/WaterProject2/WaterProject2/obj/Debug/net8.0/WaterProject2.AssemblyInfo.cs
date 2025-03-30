36ff70af6b35f0642c2a439ef0b24a58767bf823
Ella Neumarker <eneumarker@gmail.com>
2025-03-30 08:05:34 -0600

    Add, Edit, Delete

--- backend/WaterProject2/WaterProject2/obj/Debug/net8.0/WaterProject2.AssemblyInfo.cs
+++ backend/WaterProject2/WaterProject2/obj/Debug/net8.0/WaterProject2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WaterProject2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f055485df57c2739fce59e5a73b898ac8d6f7cb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b94f26db4a16af3beee05214511d58873d2ebc98")]
 [assembly: System.Reflection.AssemblyProductAttribute("WaterProject2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WaterProject2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
